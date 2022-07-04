b17cedab52b4e31de8b1ea448a05a55e1ecb0302
hasibulislam999 <hasib143sl@gmail.com>
2022-07-04 10:30:22 +0600

    mongodb added within index file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 var cors = require('cors')
 const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
 const app = express();
@@ -12,9 +13,29 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// mongodb connectivity
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@creative-agency.gsy16.mongodb.net/?retryWrites=true&w=majority`;
+const database = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+    try{
+        await database.connect();
+
+        const collection = database.db('test').collection('devices');
+        console.log('Creative agency server successfully connected!');
+    } finally{
+        // await database.close();
+    }
+} run().catch(console.dir);
+
+
 // enable requests
 app.get('/', (req, res) => {
-    res.send('Creative agency server Connected');
+    res.send(`<h1>Creative agency server Connected.</h1>`);
 })
 
 app.listen(port, () => {
